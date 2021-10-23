84224113dca0d1f23ea91adcdf508c8df88de212
Zevev <magnusfa@live.no>
2021-10-23 17:03:49 +0200

    Updating Main file for testing how getting data from user and saving them will look

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,36 @@ import model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         List<Item> items = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
         Item Vase = new Item(1,1,"Kola","Vase","FintBildet",299.99);
         items.add(Vase);
         Store store = new Store(1,"Halden Store",items);
         User user = new User("UserName", "PassWord");
 
-        System.out.println(Vase);
-        System.out.println(store);
-        System.out.println(user);
+        System.out.println("ItemID: ");
+        String itemId = sc.next();
+        int newItemID = Integer.valueOf(itemId);
+        System.out.println("ItemStore: ");
+        String ItemStore = sc.next();
+        int newItemStore = Integer.valueOf(ItemStore);
+        System.out.println("ItemName: ");
+        String ItemName = sc.next();
+        System.out.println("ItemType: ");
+        String itemType = sc.next();
+        System.out.println("ItemPictureURL: ");
+        String pictureURL = sc.next();
+        System.out.println("ItemPrice: ");
+        String itemPrice = sc.next();
+        double newItemPrice = Double.parseDouble(itemPrice);
+        items.add(new Item(newItemID,newItemStore,ItemName,itemType, pictureURL,newItemPrice));
+
+        System.out.println(items);
+
+
     }
 }
